8ff6cd0ce95db75c020bd32837d5c5bb369d1ab6
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2018-10-03 10:41:16 +0200

    Be a bit more cautious about the last request id

--- onegov/file/sign/swisscom_ais.py
+++ onegov/file/sign/swisscom_ais.py
@@ -7,11 +7,23 @@ from uuid import uuid4
 
 
 class AIS(AISBase):
-    """ Customises AIS to always remember the last issued request-id. """
+    """ Customises AIS to always remember the last issued request_id. """
 
     def _request_id(self):
         self.last_request_id = uuid4().hex
         return self.last_request_id
+
+    def post(self, payload):
+
+        # this should always hold true, unless a new release of AIS.py
+        # changes something at which point we'd have to have a look
+        #
+        # see also https://github.com/camptocamp/AIS.py/issues/7
+        #
+        if self.last_request_id not in payload:
+            raise RuntimeError("Unexpected AIS Payload")
+
+        return super().post(payload)
 
 
 class SwisscomAIS(SigningService, service_name='swisscom_ais'):
